044b30b67c5a3967338f51d5acd4d60ce042557b
Ivan Reshetnikov <resivalex@gmail.com>
2018-10-05 01:41:16 +0300

    Remove unused code

--- src/__mocks__/retrieveInitialValue.js
+++ src/__mocks__/retrieveInitialValue.js
@@ -1,15 +1,3 @@
-import http from 'http'
-
-function request(url) {
-  return new Promise((resolve) => {
-    http.get({ path: url }, (response) => {
-      let data = ''
-      response.on('data', (chunk) => (data += chunk))
-      response.on('end', () => resolve(data))
-    })
-  })
-}
-
 export default function retrieveInitialValue(index) {
   return new Promise((resolve) => {
     resolve(3 * index + 15)
